267a2fda846630863c4e6ef85c088e3f0fdb2c4d
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-01-24 22:24:05 +0600

    Create ReturnFunction.js

---
+++ ReturnFunction.js
@@ -0,0 +1,15 @@
+function add(num1, num2)
+{
+	var result = num1 + num2;
+	return result;
+}
+ var a = add(5, 110);
+ var b = add(5, 310);
+ var c = add(5, 140);
+ var d = add(5, 160);
+ var e = add(5, 510);
+document.write("result is : " + a + "</br>");
+document.write("result is : " + b + "</br>");
+document.write("result is : " + c + "</br>");
+document.write("result is : " + d + "</br>");
+document.write("result is : " + e + "</br>");
