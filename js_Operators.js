4db3d4dc946d07b63814fa4e223f7761f6707f4b
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-02-28 22:19:34 +0600

    Create js_Operators.js

---
+++ js_Operators.js
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h2>JavaScript Operators</h2>
+
+
+<p id="demo"></p>
+
+<script>
+var x = 5;
+var y = 2;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
