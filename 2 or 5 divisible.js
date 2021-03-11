d719d94aeb73a39795d9adb4d22c34083e42dd57
Md. Shihab Mahmud <shihab.diu.cse.49@gmail.com>
2021-03-11 19:34:17 +0600

    Create 2 or 5 divisible.js

---
+++ 2 or 5 divisible.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Javascript Code</title>
+</head>
+<body>
+
+<script>    var N = parseInt(prompt("Enter any number "));
+    if(N % 2==0 && N % 5==0)
+    {
+        alert("No")
+    }
+    else if(N % 2==0)
+    {
+        alert("Yes")
+    }
+    else if(N % 5==0)
+    {
+        alert("Yes")
+    }
+    else{
+        alert("No")
+    }</script>
+
+
+</body>
+</html>
